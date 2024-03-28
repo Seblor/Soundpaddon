1755154f3112d61ee3a1993ed5ad5c8944e454ad
Seblor <github@seblor.fr>
2024-03-28 20:11:19 +0100

    moved code from +layout to +page for /mobile route

--- src/routes/mobile/+layout.svelte
+++ src/routes/mobile/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-  import Toolbar from "../../components/toolbar.svelte";
   import { onMount } from "svelte";
   import { sounds } from "../../stores/sounds";
-  import Mirror from "./layouts/mirror.svelte";
-  import { isConnected, socket, soundpadClient } from "../../client/connections";
-  import { selectedLayout, serverHost } from "../../stores/settings";
-  import Customized from "./layouts/customized.svelte";
-  import AskServerIp from "./layouts/ask-server-ip.svelte";
+  import { socket, soundpadClient } from "../../client/connections";
 
   socket.on("sounds", (soundsList) => {
     console.log(soundsList);
@@ -20,20 +15,4 @@
   });
 </script>
 
-<div class="flex flex-col justify-between w-[100vw] h-full">
-  <div class="flex grow shrink overflow-hidden mb-20">
-    {#if $isConnected}
-      {#if $selectedLayout === "mirror"}
-        <Mirror />
-      {:else}
-        <Customized />
-      {/if}
-    {:else}
-      <AskServerIp />
-    {/if}
-    <slot />
-  </div>
-  <div class="w-full fixed bottom-0 highlighted-bar h-20">
-    <Toolbar />
-  </div>
-</div>
+<slot />
--- src/routes/mobile/+page.svelte
+++ src/routes/mobile/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+    import { isConnected } from "../../client/connections";
+    import Toolbar from "../../components/toolbar.svelte";
+    import { selectedLayout } from "../../stores/settings";
+    import AskServerIp from "./layouts/ask-server-ip.svelte";
+    import Customized from "./layouts/customized.svelte";
+    import Mirror from "./layouts/mirror.svelte";
+
 </script>
 
-<!-- <div>{$value}</div> -->
-<!-- <div>{$data.playedPercentage}</div> -->
-<div class="grid grid-cols-5">
+
+<div class="flex flex-col justify-between w-[100vw] h-full">
+  <div class="flex grow shrink overflow-hidden mb-20">
+    {#if $isConnected}
+      {#if $selectedLayout === "mirror"}
+        <Mirror />
+      {:else}
+        <Customized />
+      {/if}
+    {:else}
+      <AskServerIp />
+    {/if}
+    <slot />
+  </div>
+  <div class="w-full fixed bottom-0 highlighted-bar h-20">
+    <Toolbar />
+  </div>
 </div>
