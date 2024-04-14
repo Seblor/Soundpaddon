006ecc1799c94c60d2918744fa0d21e33bf6f890
Seblor <github@seblor.fr>
2024-04-14 02:00:05 +0200

    Removed previous "host" subdirectory

--- host/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import registerDataRoutes from './data'
-import registerSoundpadRoutes from './soundpad'
-import { type Application } from 'express'
-
-export const registerRoutes = (app: Application) => {
-  registerDataRoutes(app)
-  registerSoundpadRoutes(app)
-}
