6185c52b1986dbe9b165f3bd1e7959d4c96eae38
Seblor <github@seblor.fr>
2024-04-16 18:28:58 +0200

    Added analytics

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { inject } from '@vercel/analytics';
   import "../app.postcss";
   // @ts-ignore
   import { pwaInfo } from "virtual:pwa-info";
   $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
+  inject();
 </script>
 
 <svelte:head>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+  import DownloadIcon from "virtual:icons/mdi/download";
 </script>
 
-<div />
+<div
+  class="flex flex-col items-center justify-center h-[100vh] w-[100vw] gap-8"
+>
+  <div class="flex items-center gap-4">
+    <img src="/icon.png" class="h-24" alt="Soundpaddon icon" />
+    <span class="text-5xl">Soundpaddon</span>
+  </div>
+  <span class="text-2xl">Enhance your Soudpad experience</span>
+  <div>
+    <a href="https://github.com/Seblor/soundpaddon/releases">
+      <button class="btn variant-filled-primary"
+        ><span><DownloadIcon /></span><span>Download</span></button
+      >
+    </a>
+  </div>
+</div>
